7276cfe8dd2539f74b03fe9552875bb18630861f
sa-js <philipthevictor@gmail.com>
2020-03-23 18:14:39 +0500

    fixed issue in infinte scroll

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useCallback, useState } from "react";
 import { StoreContext } from "./context/store/storeContext";
-import useInfiniteScroll from "./useInfiniteScroll";
 
 const App = () => {
   const [productsCount, setProductsCount] = useState(1);
   const [products, setProducts] = useState([]);
-  const [isFetching, setIsFetching] = useInfiniteScroll(setUpdatedProductItems);
-	const { state, actions } = useContext(StoreContext);
+  const [isFetching, setIsFetching] = useState(false);
+  const { state, actions } = useContext(StoreContext);
+  
 	const computeDaysDifference = (date) => {
     let date1 = new Date(date); 
     let date2 = new Date(); 
@@ -27,15 +27,36 @@ const App = () => {
     return result
   }
 
-  function setUpdatedProductItems() {
-    setProductsCount(productsCount => productsCount+1);
-    setProducts(state => ({ ...state, products:  state.generalStates.products }));
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  function handleScroll() {
+    if (!actions.generalActions.isLoading && window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+      setIsFetching(true)
+      setProductsCount(productsCount=>productsCount+1)
+    }
+    else {
+      setIsFetching(false)
+    } 
   }
+
+  useEffect(() => {
+    if (!isFetching) return;
+    actions.generalActions.allProductsAction(productsCount)
+  }, [isFetching]);
+
+  useEffect(() => {
+    if (state.generalStates.products !== undefined) {
+      setProducts(state.generalStates.products)
+    }
+  })
 
   const fetchProducts = useCallback(() => {
       actions.generalActions.setLoading(true)
-      actions.generalActions.allProductsAction(productsCount)
-  }, [actions.generalActions.products, actions.generalActions.isLoading, productsCount,isFetching]);
+      actions.generalActions.allProductsAction(0)
+  }, [actions.generalActions.products, actions.generalActions.isLoading]);
   
   useEffect(()=>{
     fetchProducts();
@@ -55,7 +76,6 @@ const App = () => {
 
   return (
     <div>
-      {console.log(products)}
       <button onClick={sortyByPrice}>
         SORT BY PRICE
       </button>
@@ -65,7 +85,7 @@ const App = () => {
       <button onClick={sortBySize}>
         SORT BY SIZE
       </button>
-      {state.generalStates.products.length > 0 ? state.generalStates.products.map((c, index) => (
+      {products.length > 0 ? products.map((c, index) => (
         <div key={index}>
           <div>
             <h2 style={{fontSize:`${c.size}px`}}>{c.face}</h2>
